95740995378b19210e6eef074f5ac48bfdeffe86
Aria Kraft <kraft.aria@gmail.com>
2020-09-07 00:48:54 +0100

    changed order of vertices

--- triangle.cpp
+++ triangle.cpp
@@ -3,8 +3,8 @@
 void display()
 {
 	glBegin(GL_TRIANGLES);
-	glVertex2f(-0.9, -0.9);
 	glVertex2f(0, 0.9);
+	glVertex2f(-0.9, -0.9);
 	glVertex2f(0.9, -0.9);
 	glEnd();
 }
@@ -13,6 +13,7 @@ int main()
 {
 	glutInitWindowSize(400, 400); 
 	glutCreateWindow("");
+	glEnable(GL_CULL_FACE);
 	glutDisplayFunc(display); 
 	glutMainLoop();
 }
